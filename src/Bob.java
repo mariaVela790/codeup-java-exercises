73b498ab47f6c3ed61d0575a061b44c903d5f8cf
Maria Vela <maria.vela790@gmail.com>
2018-11-08 15:20:15 -0600

    talked to bob

---
+++ src/Bob.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Would you like to talk to Bob?");
+//        String talkToBob = scanner.next();
+//        scanner.nextLine();
+
+        String talkToBob;
+
+        do{
+            System.out.println("Say something to Bob: ");
+            String input = scanner.nextLine();
+
+            if(input.endsWith("?")){
+                System.out.println("Sure.");
+
+            } else if(input.endsWith("!")){
+
+                System.out.println("Whoa, chill out!");
+
+            } else if(input.isEmpty()){
+
+                System.out.println("Fine. Be that way!");
+            } else {
+
+                System.out.println("Whatever.");
+            }
+
+            System.out.println("\nWould you like to talk to Bob?");
+            talkToBob = scanner.next();
+            scanner.nextLine();
+
+        }while(talkToBob.equals("yes"));
+
+//        while(talkToBob.equals("yes")){
+//            System.out.println("Say something to Bob: ");
+//            String input = scanner.nextLine();
+//
+//            if(input.endsWith("?")){
+//                System.out.println("Sure.");
+//
+//            } else if(input.endsWith("!")){
+//
+//                System.out.println("Whoa, chill out!");
+//
+//            } else if(input.isEmpty()){
+//
+//                System.out.println("Fine. Be that way!");
+//            } else {
+//
+//                System.out.println("Whatever.");
+//            }
+//
+//            System.out.println("\nWould you like to talk to Bob?");
+//            talkToBob = scanner.next();
+//            scanner.nextLine();
+//        }
+
+    }
+}
