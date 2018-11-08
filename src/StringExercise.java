ca33f4e9af0edf1a01dad48243c2cad1cd84a432
Maria Vela <maria.vela790@gmail.com>
2018-11-08 16:45:37 -0600

    erased escape character for first message string

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,9 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String message = "We don\'t need no education\nWe don\'t need no thought control";
+        String message = "We don't need no education\nWe don't need no thought control";
         message = "Check \"this\" out!, \"s inside of \"s!";
         message = "In windows, the main drive is usually C:\\";
-        message = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        message = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
 
         System.out.println(message);
     }
