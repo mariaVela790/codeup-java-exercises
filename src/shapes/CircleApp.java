3506517338ee9d02013f29f6581d6e6622e3afce
Maria Vela <maria.vela790@gmail.com>
2018-11-13 15:56:08 -0600

    edited input constructor

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,13 +2,15 @@ package shapes;
 
 import util.Input;
 
+import java.util.Scanner;
+
 public class CircleApp {
     public static void main(String[] args) {
 
         createCircles();
     }
     public static void createCircles(){
-        Input input = new Input();
+        Input input = new Input(new Scanner(System.in));
         int numOfCircles = 0;
         boolean continueUser;
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,11 +1,15 @@
 package util;
 
+import java.util.Scanner;
+
 public class InputTest {
 
     public static void main(String[] args) {
-        Input input = new Input();
-        System.out.println(input.getString());
-        System.out.println(input.yesNo("Do you want to continue"));
-        input.getInt(1, 10);
+        Scanner sc = new Scanner(System.in);
+        Input input = new Input(sc);
+//        System.out.println(input.getString());
+//        System.out.println(input.yesNo("Do you want to continue"));
+        System.out.println(input.getInt(1, 10));
+
     }
 }
