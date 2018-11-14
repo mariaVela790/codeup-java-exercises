b2607c6892d9c6b21c186e1b4bc058cec8c45853
Maria Vela <maria.vela790@gmail.com>
2018-11-14 09:23:36 -0600

    began making a car

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+//  1. add three instance variables set to private of at least two different primitive types
+    private String carName;
+    private int mileage;
+    private boolean hasPowerWindows;
+//  2. add 2 instance methods that effect one or more instance variables
+
+    public void setPowerWindows(boolean hasPowerWindows){
+        this.hasPowerWindows = hasPowerWindows;
+    }
+//  3. add 1 static constant that makes sense for the given class
+//  4. add 1 static method that makes sense for the given class
+//  5. add an empty constructor and a constructor that sets all instance variables
+//  6. add getters and setters for all instance variables
+//  7. add main method to create the objects
+
+}
