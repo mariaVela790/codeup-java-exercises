d6d46e5247cf4461eb74265a238d9d2ff50aefe2
Maria Vela <maria.vela790@gmail.com>
2018-11-16 16:59:08 -0600

    added interface exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,21 +1,27 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
+
+
 
     public Square(double side){
         super(side, side);
     }
 
-    public double getPerimeter(){
-        return 100 * this.length;
+    public void setLength(double length){
+        this.length = length;
+        this.width = length;
+    }
+    public void setWidth(double width){
+        this.width = width;
+        this.length = width;
     }
 
     public double getArea(){
         return Math.pow(this.length, 2);
     }
-
-    public double getSide(){
-        return this.length;
+    public double getPerimeter(){
+        return (this.length * 4);
     }
 
 }
